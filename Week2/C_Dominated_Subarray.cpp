3ff08561077fcf497ba3632001ec4994a6fda7fe
Gurnoor Sohal <gnrsohal@gmail.com>
2025-06-27 13:11:51 +0530

    Add files via upload

---
+++ Week2/C_Dominated_Subarray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define ll long long
+using namespace std;
+
+int main() {
+    ll t; cin>>t;
+    while(t--) {
+        ll n; cin>>n;
+        vector<ll> a(n);
+        for(ll i=0; i<n; i++) cin>>a[i];
+        
+    }        
+}
